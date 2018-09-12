0a7e24d74dca98ee59d86cd90b676c00fc903f14
蔡昀達 <b04902103@ntu.edu.tw>
2018-09-12 16:17:14 +0800

    add relu
    relu++

--- src/malconv.py
+++ src/malconv.py
@@ -7,10 +7,11 @@ def Malconv(max_len=200000, win_size=500, vocab_size=256):
 
     conv1 = Conv1D(kernel_size=(win_size), filters=128, strides=(win_size), padding='same')(emb)
     conv2 = Conv1D(kernel_size=(win_size), filters=128, strides=(win_size), padding='same')(emb)
-    a = Activation('sigmoid')(conv2)
+    a = Activation('sigmoid', name='sigmoid')(conv2)
     
     mul = multiply([conv1, a])
-    p = GlobalMaxPool1D()(mul)
+    a = Activation('relu', name='relu')(mul)
+    p = GlobalMaxPool1D()(a)
     d = Dense(64)(p)
     out = Dense(1, activation='sigmoid')(d)
 
